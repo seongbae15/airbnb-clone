de742c84f1aa227cdd4958b71c31f9718e1195e2
seongbae <pseongbae@gmail.com>
2025-06-28 02:10:59 +0900

    Create custom user model

--- houses/admin.py
+++ houses/admin.py
@@ -5,7 +5,6 @@ from .models import House
 # Register your models here.
 @admin.register(House)
 class HouseAdmin(admin.ModelAdmin):
-    fields = ("name", "address", ("price_per_night", "pets_allowed"))
     list_display = (
         "name",
         "price_per_night",
@@ -16,5 +15,3 @@ class HouseAdmin(admin.ModelAdmin):
         "price_per_night",
         "pets_allowed",
     )
-    search_fields = ("address",)
-    list_editable = ("pets_allowed",)
