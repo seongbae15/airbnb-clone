173a2ef99b406b69af7b3318214562d1443f6028
seongbae <pseongbae@gmail.com>
2025-07-02 01:47:58 +0900

    Update room model

--- rooms/models.py
+++ rooms/models.py
@@ -1,9 +1,31 @@
 from django.db import models
+from common.models import CommonModel
 
 
 # Create your models here.
-class Room(models.Model):
+class Room(CommonModel):
     "Room model definition"
+
+    class RoomKindChoices(models.TextChoices):
+        ENTIRE_PLACE = ("entire_place", "Entire Place")
+        PRIVATE_ROOM = ("private_room", "Private Room")
+        SHARED_ROOM = ("shared_room", "Shared Room")
 
     country = models.CharField(max_length=50, default="South Korea")
     city = models.CharField(max_length=80, default="Seoul")
+    price = models.PositiveIntegerField()
+    rooms = models.PositiveIntegerField()
+    toilets = models.PositiveIntegerField()
+    description = models.TextField()
+    address = models.CharField(max_length=250)
+    pet_friendly = models.BooleanField(default=True)
+    kind = models.CharField(max_length=20, choices=RoomKindChoices.choices)
+    owner = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    amenities = models.ManyToManyField("rooms.Amenity", blank=True)
+
+
+class Amenity(CommonModel):
+    "Amenity model definition"
+
+    name = models.CharField(max_length=150)
+    description = models.CharField(max_length=150, null=True, blank=True)
