c653a74d3092682ee999bb69ee3e1e98d3e9d379
seongbae <pseongbae@gmail.com>
2025-07-05 20:43:59 +0900

    Add medias

---
+++ medias/models.py
@@ -0,0 +1,38 @@
+from django.db import models
+from common.models import CommonModel
+
+
+class Photo(CommonModel):
+    file = models.ImageField()
+    description = models.CharField(
+        max_length=140,
+    )
+    room = models.ForeignKey(
+        "rooms.Room",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+    experience = models.ForeignKey(
+        "experiences.Experience",
+        null=True,
+        blank=True,
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return "Photo file"
+
+
+class Video(CommonModel):
+    file = models.FileField()
+    description = models.CharField(
+        max_length=140,
+    )
+    experience = models.OneToOneField(
+        "experiences.Experience",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return "Video file"
