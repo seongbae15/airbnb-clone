a73b23d6f7b59d74022ba5db81511ce93b16d7ca
seongbae <pseongbae@gmail.com>
2025-07-07 02:37:19 +0900

    Django Templates

--- rooms/views.py
+++ rooms/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Room
 
 
 # Create your views here.
 def see_all_rooms(request):
-    return HttpResponse("See all rooms")
+    rooms = Room.objects.all()
+    return render(request, "all_rooms.html", {"rooms": rooms})
 
 
 def see_one_room(request, room_id):
