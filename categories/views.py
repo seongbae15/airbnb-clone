79968dfa878d3911060880d747561123fef117ba
seongbae <pseongbae@gmail.com>
2025-07-08 10:44:58 +0900

    Add api_view

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from .models import Category
+from django.http import JsonResponse
+from django.core import serializers
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 
-# Create your views here.
+
+@api_view()
+def categories(request):
+    return Response(
+        {
+            "ok": True,
+            "categories": Category.objects.all(),
+        }
+    )
