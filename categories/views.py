877cc174217437686afadaba52214b9aa8b6644a
seongbae <pseongbae@gmail.com>
2025-07-09 01:34:10 +0900

    DELETE

--- categories/views.py
+++ categories/views.py
@@ -3,7 +3,9 @@ from .models import Category
 from django.http import JsonResponse
 from django.core import serializers
 from rest_framework.decorators import api_view
+from rest_framework.exceptions import NotFound
 from rest_framework.response import Response
+from rest_framework.status import HTTP_204_NO_CONTENT
 from categories.serializers import CategorySerializer
 
 
@@ -29,8 +31,30 @@ def categories(request):
             return Response(serializer.errors)
 
 
-@api_view()
+@api_view(["GET", "PUT", "DELETE"])
 def category(request, pk):
-    category = Category.objects.get(pk=pk)
-    serializer = CategorySerializer(category)
-    return Response(serializer.data)
+    try:
+        category = Category.objects.get(pk=pk)
+    except Category.DoesNotExist:
+        raise NotFound
+
+    if request.method == "GET":
+        serializer = CategorySerializer(category)
+        return Response(serializer.data)
+    elif request.method == "PUT":
+        serializer = CategorySerializer(
+            category,
+            data=request.data,
+            partial=True,
+        )
+        if serializer.is_valid():
+            updated_serializer = serializer.save()
+            return Response(
+                CategorySerializer(updated_serializer).data,
+            )
+        else:
+            return Response(serializer.errors)
+
+    elif request.method == "DELETE":
+        category.delete()
+        return Response(status=HTTP_204_NO_CONTENT)
