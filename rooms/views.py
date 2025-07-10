47c7cebc8d240ec9ce7a0d6d3223d9e968215f34
seongbae <pseongbae@gmail.com>
2025-07-11 01:33:29 +0900

    All amenities

--- rooms/views.py
+++ rooms/views.py
@@ -1,6 +1,34 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-from .models import Room
+from rest_framework.response import Response
+from .serializers import AmenitySerializer
+from rest_framework.views import APIView
+from .models import Amenity
+
+
+class Amenities(APIView):
+    def get(self, request):
+        all_amenities = Amenity.objects.all()
+        serializer = AmenitySerializer(all_amenities, many=True)
+        return Response(serializer.data)
+
+    def post(self, request):
+        serializer = AmenitySerializer(data=request.data)
+        if serializer.is_valid():
+            amenity = serializer.save()
+            return Response(AmenitySerializer(amenity).data)
+        else:
+            return Response(serializer.errors)
+
+
+class AmenityDetail(APIView):
+    def get(self, request, pk):
+        pass
+
+    def put(self, request, pk):
+        pass
+
+    def delete(self, request, pk):
+        pass
 
 
 # Create your views here.
